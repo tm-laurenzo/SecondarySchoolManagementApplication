03500d871f9e11aaaf66ac825d4a6c413a7edfe8
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2023-05-18 21:37:15 +0100

    deleted weather forcast controller, updated todos

--- SSMA.Models/Models/Todos.cs
+++ SSMA.Models/Models/Todos.cs
@@ -10,5 +10,6 @@ namespace SSMA.Models.Models
     {
         //Todo: Accounts for financial transaction, part of the microservice
         //Non-teaching staff should be in their own microservice
+        //May 16 implement repositories and unit of work
     }
 }
