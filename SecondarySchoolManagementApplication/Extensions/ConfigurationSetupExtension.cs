9b792d04f42c22a4a7eba87ab6b2edbb6c9a66d4
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2023-05-26 23:58:59 +0100

    finalized config extension with secrets

--- SecondarySchoolManagementApplication/Extensions/ConfigurationSetupExtension.cs
+++ SecondarySchoolManagementApplication/Extensions/ConfigurationSetupExtension.cs
@@ -4,7 +4,7 @@
     {
         public static IConfiguration GetConfig(bool isDevelopment)
         {
-            string path = @"c:\users\chidolue lawrence\AppData\Roaming\Microsoft\UserSecrets\5e1b2274-ba4c-4918-bdc1-d2939a4a540d";
+            string path = @"C:\Users\Chidolue Lawrence\AppData\Roaming\Microsoft\UserSecrets\bac9d45e-fb5a-417b-bb16-676e899579bd";
             return isDevelopment ? new ConfigurationBuilder()
                    .SetBasePath(path)
                    .AddJsonFile("secrets.json")
