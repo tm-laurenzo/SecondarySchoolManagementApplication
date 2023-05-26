2a6f28822a1913042fa9d4acb3712e4d1df5ddb9
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2023-05-26 23:12:11 +0100

    added some stuff

---
+++ SecondarySchoolManagementApplication/Extensions/ConfigurationSetupExtension.cs
@@ -0,0 +1,14 @@
+﻿namespace SecondarySchoolManagementApplication.Extensions
+{
+    public static class ConfigurationSetupExtension
+    {
+        public static IConfiguration GetConfig(bool isDevelopment)
+        {
+            return isDevelopment ? IConfiguration
+                :
+                new ConfigurationBuilder()
+                   .AddEnvironmentVariables()
+                   .Build();
+        }
+    }
+}
