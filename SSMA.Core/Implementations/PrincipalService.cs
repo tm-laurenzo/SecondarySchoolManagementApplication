af0c707d5891caf86cc7c964b889594d208d18e8
tm-laurenzo <60081627+tm-laurenzo@users.noreply.github.com>
2023-06-05 08:34:43 +0100

    started working on the core

---
+++ SSMA.Core/Implementations/PrincipalService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSMA.Core.Implementations
+{
+    public class PrincipalService
+    {
+    }
+}
